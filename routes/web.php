4f8b2619aae2c31cf2509da601005bd3c482e67c
oswaldoR12 <61508313+oswaldoR12@users.noreply.github.com>
2025-09-05 00:38:24 -0400

    web.php

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,11 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 
 // Rutas públicas
-Route::get('/', [tu ::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 Route::get('/projects', [HomeController::class, 'projects'])->name('projects');
 Route::get('/contact', [ContactController::class, 'show'])->name('contact.show');
-
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-
 
 // Rutas de administración
 Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () {
